53eee230fa1d2cee182bdb215625b23c5b28b531
Rodrigo Silva <rodrigooosc@outlook.com>
2022-04-01 16:39:01 -0300

    Criando projeto

---
+++ wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+// Efeito de máquina de escrever
+function typeWriter(texto){   
+    const textoArray = texto.innerText.split('');   
+    
+    texto.innerHTML = '';
+
+    textoArray.forEach((letra, i ) => {
+        setTimeout(() => 
+            texto.innerHTML += letra, 85 * i)        
+    });       
+}
+
+function efeitoMaquinaEscrever(titulo, texto, callback){
+    typeWriter(titulo);
+    callback(typeWriter(texto));   
+}
+
+const titulo = document.querySelector('.titulo-home');
+const texto = document.querySelector('.texto-home');
+
+// document.querySelector('.titulo-home').innerHTML = '';
+document.querySelector('.texto-home').innerHTML = '';
+
+efeitoMaquinaEscrever(titulo,texto);
+
+
+
+
+
+
+
+
+
+
+
