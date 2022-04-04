657b6489f7284e79cfe3aec13e9673a52b66dbb9
Rodrigo Silva <rodrigooosc@outlook.com>
2022-04-04 13:37:12 -0300

    Criação do Controller e do Model (PessoaController e PessoaModel)

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cadastro_MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace Cadastro_MVC.Controllers
+{
+    public class PessoaController : Controller
+    {
+        // GET: Pessoa
+        public ActionResult Index()
+        {
+            return View();
+        }
+ 
+        // GET: Pessoa/Details/id
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+ 
+        // GET: Pessoa/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //A View agora está relacionada com o Modelo
+            return View(new PessoaModel());
+           
+        }
+ 
+        // POST: Pessoa/Create
+        [HttpPost]
+        public ActionResult Create(PessoaModel pModel)
+        {
+            pModel.Salvar();
+ 
+            return RedirectToAction("Index");
+        }
+ 
+        // GET: Pessoa/Edit/id
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+ 
+        // POST: Pessoa/Edit/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+ 
+        // GET: Pessoa/Delete/id
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+ 
+        // POST: Pessoa/Delete/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        } 
+
+          
+
+    }
+}
---
+++ Models/PessoaModel.cs
@@ -0,0 +1,48 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Cadastro_MVC.Models
+{
+    public class PessoaModel
+    {
+        // Campos (banco de dados) ou atributos desta classe
+        [DisplayName("ID")] // É semelhante a uma etiqueta ou Label/Tag (APELIDO)
+        public int PessoaID { get; set; }
+        [DisplayName("Nome")]
+        public string PessoaNome { get; set; }
+        [DisplayName("Email")]
+        public string PessoaEmail { get; set; }
+        [DisplayName("Telefone")]
+        public string PessoaTelefone { get; set; }
+
+        // Criar uma CONSTANTE para conexão com o banco de dados
+        readonly string connectionString = @"Server=localhost\SQLEXPRESS;Database=cadastro_mvc;Trusted_Connection=True";          
+
+        // Este método salva os dados que vieram do formulário no banco de dados.          
+        public void Salvar()
+        {
+            // Cria uma conexão com o banco de dados
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {            
+                // Realiza a abertura de uma conexão com o banco de dados
+                sqlCon.Open();
+
+                // Cria uma instrução SQL para ser executada no servidor SQL Server
+                SqlCommand sqlCmd = new SqlCommand("INSERT INTO tb_pessoa VALUES (@PessoaNome, @PessoaEmail, @PessoaTelefone)", sqlCon);
+
+                sqlCmd.Parameters.AddWithValue("@PessoaNome", PessoaNome);
+                sqlCmd.Parameters.AddWithValue("@PessoaEmail", PessoaEmail);
+                sqlCmd.Parameters.AddWithValue("@PessoaTelefone", PessoaTelefone);
+
+                //Executar o comando no SQL (Tecla F5 do SQL Server)
+                sqlCmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
